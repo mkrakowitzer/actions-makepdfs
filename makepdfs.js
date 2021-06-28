f0730c0186d30a0117461c0fcdf8a0d9ef9ce669
Chris <christopher.king@nelnet.com>
2021-06-28 16:04:00 -0500

    Add image support

--- makepdfs.js
+++ makepdfs.js
@@ -2,14 +2,16 @@
 
 const markdown_dir = process.env.INPUT_MARKDOWN_DIR;
 const output_dir = process.env.INPUT_OUTPUT_DIR;
+const media_dir = process.env.INPUT_MEDIA_DIR;
 
 'use strict';
 var fs = require( 'fs' );
 var mddir = '/github/workspace/' + markdown_dir;
 var dir = '/github/workspace/' + output_dir + '/';
+var meddir = '/github/workspace' + media_dir + '/';
 
-/* 
- * Show an error message 
+/*
+ * Show an error message
  */
 function showErrorMessage(msg, error) {
 	console.log('ERROR: ' + msg);
@@ -28,7 +30,7 @@ function makeHtml(data) {
 		var template = fs.readFileSync("/template/template.html").toString('utf-8');
 		// compile template
 		var mustache = require('mustache');
-  
+
 		var view = {
 			style: style,
 			content: data
@@ -46,7 +48,14 @@ function makePdf(data,file) {
 	try {
 		file  = file.replace('.md','');
 		const puppeteer = require('puppeteer');
+    const express = require('express');
 		(async () => {
+
+        //Create server for images
+        const app = express();
+        app.use(express.static(meddir));
+        server = app.listen(3000);
+
 				const browser = await puppeteer.launch( {
 				executablePath:'/node_modules/puppeteer/.local-chromium/linux-782078/chrome-linux/chrome',
 				args: [
@@ -66,8 +75,9 @@ function makePdf(data,file) {
 				footerTemplate: '<div class="pageNumber" style="font-size:8px;width:100%;text-align:center;"></div>',
 				headerTemplate: '<div class="date" style="font-size:8px;width:100%;text-align:center;"></div>'
 			});
-		  
+
 			await browser.close();
+      await server.close();
 		  })();
 	} catch (error) {
 		showErrorMessage('makeHtml()', error);
@@ -94,9 +104,13 @@ if (!fs.existsSync(dir)){
     fs.mkdirSync(dir);
 }
 
+if (!fs.existsSync(meddir)){
+    fs.mkdirSync(meddir);
+}
+
 fs.readdir (mddir,function(err, files) {
    for (let file of files) {
-      if (path.extname(file) == '.md') {				 
+      if (path.extname(file) == '.md') {
 				var text = fs.readFileSync(mddir + '/' + file).toString('utf-8');
 				var md = require('markdown-it')({
 					html: true,
@@ -107,7 +121,7 @@ fs.readdir (mddir,function(err, files) {
 							str = hljs.highlight(lang, str, true).value;
 						} catch (error) {
 							str = md.utils.escapeHtml(str);
-				
+
 							showErrorMessage('markdown-it:highlight', error);
 						}
 						} else {
